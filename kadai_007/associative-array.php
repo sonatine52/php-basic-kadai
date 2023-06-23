0c9c38d5e87f5cdcf0b056bab2b30b0eba214d83
sonatine52 <135916531+sonatine52@users.noreply.github.com>
2023-06-23 12:11:55 +0900

    Add files via upload

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>7章課題</title>
+    </head>
+
+    <body>
+        <p>
+            <?php
+            $foods = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+            print_r($foods);
+            ?>
+        </p>
+    </body>
+</html>
